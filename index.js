180ecc79e434b01e6c8defcb8f0db4ec6e468573
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-01-03 15:20:15 +0100

    Build: Allow resolution selection

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const etchASketch = document.querySelector(".etch-a-sketch");
+const resolution = document.querySelector("#resolution");
+const rainbow = document.querySelector("#rainbow");
 
 for (let i = 0; i < 16; i++) {
 	for (let j = 0; j < 16; j++) {
@@ -16,3 +18,33 @@ for (let i = 0; i < 16; i++) {
 		etchASketch.appendChild(pixel);
 	}
 }
+
+resolution.addEventListener("click", () => {
+	let num = +prompt("What pixel resolution would you like?", "");
+	while (num > 100) {
+		num = +prompt("Please pick a number less than 100!", "");
+	}
+
+	while (etchASketch.lastElementChild) {
+		const child = etchASketch.lastElementChild;
+		etchASketch.removeChild(child);
+		child.remove();
+	}
+
+	for (let i = 0; i < num; i++) {
+		for (let j = 0; j < num; j++) {
+			const pixel = document.createElement("div");
+
+			pixel.setAttribute("class", "pixel");
+			pixel.setAttribute("id", "pixel" + i + j);
+			pixel.style.height = 50 / num + "vh";
+			pixel.style.width = 50 / num + "vh";
+
+			pixel.addEventListener("mouseover", (e) => {
+				e.target.style.background = "black";
+			});
+
+			etchASketch.appendChild(pixel);
+		}
+	}
+});
