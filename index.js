aeafe1e0524e3633b1f43379f219beb730fcb8dc
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-01-03 14:51:41 +0100

    Build: Change color on hover

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const container = document.querySelector(".container");
 for (let i = 0; i < 16; i++) {
 	for (let j = 0; j < 16; j++) {
 		const pixel = document.createElement("div");
+
 		pixel.setAttribute("class", "pixel");
+		pixel.setAttribute("id", "pixel" + i + j);
 		pixel.style.height = 100 / 16 + "vh";
 		pixel.style.width = 100 / 16 + "vw";
+
+		pixel.addEventListener("mouseover", (e) => {
+			e.target.style.background = "black";
+		});
+
 		container.appendChild(pixel);
 	}
 }
