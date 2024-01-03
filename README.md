f54eb9d8ddb1cf3e4639ee7c65d3f61283b21e26
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-01-03 15:36:06 +0100

    Build: Allow rainbow color option

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # etch-a-sketch
+
 Etch-a-Sketch website using HTML, CSS and JavaScript
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const etchASketch = document.querySelector(".etch-a-sketch");
 const resolution = document.querySelector("#resolution");
 const rainbow = document.querySelector("#rainbow");
+const black = document.querySelector("#black");
+
+let color = "black";
+etchASketch.addEventListener("mouseover", (e) => {
+	e.target.style.background = "black";
+});
 
 for (let i = 0; i < 16; i++) {
 	for (let j = 0; j < 16; j++) {
@@ -11,12 +17,25 @@ for (let i = 0; i < 16; i++) {
 		pixel.style.height = 50 / 16 + "vh";
 		pixel.style.width = 50 / 16 + "vh";
 
-		pixel.addEventListener("mouseover", (e) => {
-			e.target.style.background = "black";
-		});
-
 		etchASketch.appendChild(pixel);
 	}
+}
+
+function getRandomColor() {
+	var letters = "0123456789ABCDEF";
+	var color = "#";
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+
+function setBlack(e) {
+	e.target.style.background = black;
+}
+
+function setRandomColor(e) {
+	e.target.style.background = getRandomColor();
 }
 
 resolution.addEventListener("click", () => {
@@ -46,5 +65,25 @@ resolution.addEventListener("click", () => {
 
 			etchASketch.appendChild(pixel);
 		}
+	}
+});
+
+rainbow.addEventListener("click", () => {
+	if (color === "rainbow") {
+		return;
+	} else {
+		color = "rainbow";
+		etchASketch.removeEventListener("mouseover", setBlack);
+		etchASketch.addEventListener("mouseover", setRandomColor);
+	}
+});
+
+black.addEventListener("click", () => {
+	if (color === "black") {
+		return;
+	} else {
+		color = "black";
+		etchASketch.removeEventListener("mouseover", setRandomColor);
+		etchASketch.addEventListener("mouseover", setBlack);
 	}
 });
